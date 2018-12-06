447a00628f717a2ffb4b3105a3f451a1a52cdd8c
Devin Hight <33430196+DHgit1@users.noreply.github.com>
2018-12-05 20:47:19 -0800

    Update UserController.java

--- src/main/java/edu/csumb/cst438/UserService/api/UserController.java
+++ src/main/java/edu/csumb/cst438/UserService/api/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
