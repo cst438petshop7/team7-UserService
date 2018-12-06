0bb8e9bf67bcee8e7c9125d59f6aabffe0fa0e51
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-12-05 23:24:57 -0800

    new tests

--- src/main/java/edu/csumb/cst438/UserService/api/UserController.java
+++ src/main/java/edu/csumb/cst438/UserService/api/UserController.java
@@ -1,14 +1,11 @@
 package edu.csumb.cst438.userservice.api;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,7 +27,7 @@ public class UserController {
     ResponseEntity<User> getUser(@RequestBody User user){
         User result = callUserDB(user.getUsername().getUsername());
         if(result==null){
-                return new ResponseEntity<User>(result,HttpStatus.NOT_FOUND);
+            return new ResponseEntity<User>(result,HttpStatus.NOT_FOUND);
         }
         if(result.getUsername().getUsername().equals(user.getUsername().getUsername()) && result.getPassword().getPassword().equals(user.getPassword().getPassword())){
             return new ResponseEntity<User> (result,HttpStatus.OK);
